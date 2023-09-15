050b448ce7911c501acb05c737b63c2f187aebae
emsi06001 <emsi06001@E-PW0234N3>
2023-09-15 07:55:35 +0200

    Add project files.

---
+++ inluppg2.5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace inluppg2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skrivi in en multiplikation");
+            string input = Console.ReadLine();
+
+            int xIndex = input.IndexOf('*');
+            int n1 = int.Parse(input[..xIndex]);
+            int n2 = int.Parse(input[(xIndex + 1)..]);
+            Console.WriteLine($"Svaret är {n1*n2}");
+            Console.ReadKey();
+        }
+    }
+}
